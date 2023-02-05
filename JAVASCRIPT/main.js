4838920648a0684b4515ff5da2ebcb1fa1a45412
www-myuniversegeek <124443850+www-myuniversegeek@users.noreply.github.com>
2023-02-05 06:34:30 -0300

    Initial commit

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,36 @@
+function clickMenu() {
+    if ( rodando.style.display == "block") {
+        rodando.style.display ="none"
+    } else {
+        rodando.style.display ="block"
+    }
+}
+
+
+function clickLogo() {
+    if ( redelogo.style.width == "270px") {
+        
+        redelogo.style.width ="200px"  
+        
+    } else {
+        redelogo.style.width ="270px"
+    }
+}
+
+
+
+function clickFechar() {
+    if ( fechar.style.display == "block") {
+        fechar.style.display ="none"
+    } else {
+        fechar.style.display ="block"
+    }
+}
+
+function mostrarAmigo() {
+        document.getElementById("").src = ""
+
+
+}
+
+
